b8f7e850113d7d2805bb50e07cae4f1c428aefe8
SNSN100 <gjzxyb@126.com>
2021-12-08 23:02:19 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -4,9 +4,14 @@ shopt -s extglob
 sed -i '/	refresh_config();/d' scripts/feeds
 
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.1.252/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Custom settings
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
 #sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
 #sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+rm -rf package/lean/luci-theme-argon
+git clone https://github.com/kenzok8/luci-theme-ifit.git package/lean/luci-theme-ifit
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
